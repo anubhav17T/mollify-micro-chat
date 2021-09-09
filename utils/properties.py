34e5ea7947417ee21499fc22dc7c857d02a3547e
Anubhav Tyagi <38591131+anubhav17T@users.noreply.github.com>
2021-09-09 16:45:04 +0530

    Add files via upload

---
+++ utils/properties.py
@@ -0,0 +1,8 @@
+DB_name = "MolifyChat"
+
+DEFAULT_EXPIRE_TIME = 7 * 24 * 60 * 60 * 1000
+FILE_SUFFIXS = ("pdf", "jpeg", "png", "jpg")
+
+keysInformation ={"mongodb.auth.enabled": 'true',
+                  "mongodb.server.url": ""}
+
